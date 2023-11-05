9b4be702293011a6f743ed219a2e60b4033eed32
Gpwallace1109 <147451469+Gpwallace1109@users.noreply.github.com>
2023-11-05 14:46:38 -0600

    RPS.js

---
+++ RPS.js
@@ -0,0 +1,69 @@
+//  Rock, Paper, or Scissors 
+
+// How to get userInput
+const getUserChoice = userInput => { 
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') { 
+        return (userInput);
+    }
+    else { 
+        return ('Error...please select rock, paper, or scissors!');
+    }
+}
+
+// How to get ComputerChoice
+const getComputerChoice = () => { 
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) { 
+        case 0: 
+        return 'rock';
+        case 1: 
+        return 'paper';
+        case 2: 
+        return 'scissors';
+    }
+};
+
+
+// How to determine the Winner 
+const determineWinner = (getUserChoice, getComputerChoice) => { 
+    if (getUserChoice === getComputerChoice) { 
+        return 'This game is a tie!';
+    } 
+    if (getUserChoice === 'rock') { 
+        if (getComputerChoice === 'paper')
+        return 'The computer won this round!';
+    } else { 
+        return 'You won this round!';
+    }
+    if (getUserChoice === 'paper') { 
+        if (getComputerChoice === 'scissors')
+        return 'The computer won this round!';
+    } else { 
+        return 'You won this round!';
+    }
+    if (getUserChoice === 'scissors') { 
+        if (getComputerChoice === 'paper')
+        return 'The computer won this round!';
+    } else { 
+        return 'You won this round!';
+    }
+    if (getUserChoie === 'bomb') { 
+        return 'You win! Bomb explodes all!'
+    }
+
+};
+
+// Now to Play Game...
+const playGame = () => { 
+    const userChoice = getUserChoice('paper'); 
+    const computerChoice = getComputerChoice();
+    console.log(`You shot ${userChoice}`); 
+    console.log(`The computer shot ${computerChoice}`);
+
+    console.log(determineWinner(userChoice, computerChoice));
+};
+
+// Call Play Game function...
+playGame(); 
+
